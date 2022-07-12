9f181e13d45741c5ca4212f666b89c335e058460
felpfsf <feferreira.sf@gmail.com>
2022-07-12 18:18:46 -0300

    setup colors and font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        palette: {
+          accent: '#0b0d17',
+          paragraph: '#d0d6f9'
+        }
+      },
+      fontFamily: {
+        barlow: ['Barlow', 'sans-serif'],
+        barlowCondensed: ['Barlow Condensed', 'sans-serif'],
+        bellefair: ['Bellefair', 'serif']
+      }
+    }
   },
   plugins: []
 }

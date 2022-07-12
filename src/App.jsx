33bb21595047435912df6603da4282f22a489f18
felpfsf <feferreira.sf@gmail.com>
2022-07-12 18:19:24 -0300

    config react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
+import { Link, Route, Routes } from "react-router-dom"
+import Crew from "./pages/Crew"
+import Destination from "./pages/Destination"
+import Home from "./pages/Home"
+import Technology from "./pages/Technology"
 
 function App() {
 
   return (
-    <div>
-      <h1 className="text-9xl text-purple-300 ">
-        Hello React
-      </h1>
-    </div>
+    <>
+      <div>
+        <Link to='/'>Home</Link>
+        <Link to='/destination'>Destination</Link>
+        <Link to='/crew'>Crew</Link>
+        <Link to='/technology'>Technology</Link>
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/destination" element={<Destination />} />
+        <Route path="/crew" element={<Crew />} />
+        <Route path="/technology" element={<Technology />} />
+      </Routes>
+    </>
   )
 }
 
 export default App
+

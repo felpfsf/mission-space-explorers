abb7cbe548a563a51dc1453dfcb3692583a81970
felpfsf <feferreira.sf@gmail.com>
2022-07-13 16:57:58 -0300

    refactoring

--- src/pages/Technology.jsx
+++ src/pages/Technology.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Technology = () => {
   return (
-    <div className='w-full h-screen bg-[url(./src/assets/technology/background-technology-mobile.jpg)] sm:bg-[url(./src/assets/technology/background-technology-tablet.jpg)] xl:bg-[url(./src/assets/technology/background-technology-desktop.jpg)] bg-cover bg-center'>Technology</div>
+    <div className='w-full h-screen bg-[url(../src/assets/technology/background-technology-mobile.jpg)] sm:bg-[url(../src/assets/technology/background-technology-tablet.jpg)] xl:bg-[url(../src/assets/technology/background-technology-desktop.jpg)] bg-cover bg-center'>Technology</div>
   )
 }
 

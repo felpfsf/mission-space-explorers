88bb4cd8b6cc064fa92b453d4450fdd0af8c25d4
felpfsf <feferreira.sf@gmail.com>
2022-07-12 23:12:45 -0300

    styleguide file

---
+++ src/typography.jsx
@@ -0,0 +1,65 @@
+<div className="bg-palette-accent text-white">
+  <p>
+    Heading 1
+  </p>
+  <h1 className="text-[150px] font-bellefair text-white ">
+    EARTH
+  </h1>
+  <hr className="mb-8" />
+  <p>
+    Heading 2
+  </p>
+  <h2 className="font-bellefair text-[100px] text-white">
+    VENUS
+  </h2>
+  <hr className="mb-8" />
+  <p>
+    Heading 3
+  </p>
+  <h3 className="font-bellefair text-[56px] text-white">
+    JUPITER & SATURN
+  </h3>
+  <hr className="mb-8" />
+  <p>
+    Heading 4
+  </p>
+  <h4 className="font-bellefair text-[32px] text-white">
+    URANUS, NEPTUNE & PLUTO
+  </h4>
+  <hr className="mb-8" />
+  <p>
+    Heading 5
+  </p>
+  <h5 className="font-barlowCondensed text-[24px] text-palette-paragraph tracking-[4.75px]">
+    SO, YOU WANT TO TRAVEL TO SPACE
+  </h5>
+  <hr className="mb-8" />
+  <p>
+    Subheading 1
+  </p>
+  <h6 className="font-bellefair text-[28px] text-white">
+    384, 400 KM
+  </h6>
+  <hr className="mb-8" />
+  <p>
+    Subheading 2
+  </p>
+  <h6 className="font-barlowCondensed text-sm text-white tracking-[2.35px]">
+    AVG. DISTANCE
+  </h6>
+  <hr className="mb-8" />
+  <p>
+    Nav Text
+  </p>
+  <p className="font-barlowCondensed text-base text-white tracking-[2.7px]">
+    EUROPA
+  </p>
+  <hr className="mb-8" />
+  <p>
+    Body
+  </p>
+  <p className="font-barlow text-lg text-white leading-8 w-[540px]">
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia veniam voluptatibus vitae in, iste molestiae quaerat alias asperiores error! Ex numquam quas molestias unde repellat praesentium natus. Velit laudantium cum porro dignissimos dolore est facere inventore? Officia possimus adipisci ab vero magnam placeat consequuntur, alias iste fugit animi voluptas suscipit? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit necessitatibus odit ipsam eligendi perferendis ad culpa dolores animi placeat temporibus, eveniet sunt quaerat praesentium maiores error fugit repellendus nisi! Illo!
+  </p>
+
+</div>

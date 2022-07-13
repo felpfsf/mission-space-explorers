cbb88cfcb0a88cf6e55238a2a8e779e00f2c3dc4
felpfsf <feferreira.sf@gmail.com>
2022-07-13 20:44:59 -0300

    Home section mobile, tablet and desktop

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,23 @@ import React from 'react'
 const Home = () => {
   return (
     <div className='w-full h-screen bg-bgHomeMobile sm:bg-bgHomeTablet xl:bg-bgHomeDesktop bg-cover bg-center'>
-      Home
+
+      <div className='xl:h-full xl:pb-32 pt-28 sm:pt-48  flex flex-col items-center justify-center xl:grid xl:grid-cols-2 xl:justify-items-center xl:items-end xl:content-end'>
+
+        <div className='w-[327px] sm:w-[450px] flex flex-col items-center xl:items-start'>
+          <h5 className='heading--5 uppercase'>
+            So, you want to travel to
+          </h5>
+          <h1 className='heading--1 uppercase'>Space</h1>
+          <p className='paragraph text-center xl:text-left'>
+            Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!
+          </p>
+        </div>
+        <div className='group w-[150px] h-[150px] sm:w-60 sm:h-60 xl:w-[272px] xl:h-[272px] z-10 mt-16 sm:mt-[150px] md:mt-32 bg-white rounded-full flex items-center justify-center cursor-pointer'>
+          <p className='buttonText'>Explore</p>
+          <div className='w-[150px] h-[150px] sm:w-60 sm:h-60 xl:w-[272px] xl:h-[272px] absolute rounded-full group-hover:scale-[1.45] group-hover:bg-white opacity-[0.1] -z-[100]'></div>
+        </div>
+      </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
+      
+      fontSize: {
+        titleDynamic: 'clamp(5rem, 3.5417rem + 6.4815vw, 9.375rem)',
+        subTitleDynamic: 'clamp(1rem, 0.8333rem + 0.7407vw, 1.75rem)',
+        paragraphDynamic: 'clamp(0.938rem, 0.8757rem + 0.2770vw, 1.125rem)',
+        navDynamic: 'clamp(0.875rem, 0.8333rem + 0.1852vw, 1rem)',
+        btnTextDynamic: 'clamp(1.25rem, 1.0000rem + 1.1111vw, 2rem)'
+      },
       backgroundImage: {
         bgHomeDesktop: 'url(/src/assets/home/background-home-desktop.jpg)',
         bgHomeTablet: 'url(/src/assets/home/background-home-tablet.jpg)',

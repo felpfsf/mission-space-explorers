b67ba83b2b7227c0f75738520953000f2f2214cd
felpfsf <feferreira.sf@gmail.com>
2022-07-14 18:04:30 -0300

    adjusting padding

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div className='w-full h-screen bg-[url("/assets/home/background-home-mobile.jpg")] sm:bg-[url("/assets/home/background-home-tablet.jpg") xl:bg-[url("/assets/home/background-home-desktop.jpg")] bg-cover bg-center'>
 
-      <div className='xl:h-full xl:pb-32 pt-28 sm:pt-48  flex flex-col items-center justify-center xl:grid xl:grid-cols-2 xl:justify-items-center xl:items-end xl:content-end'>
+      <div className='xl:h-full xl:pb-32 pt-28 sm:pt-40  flex flex-col items-center justify-center xl:grid xl:grid-cols-2 xl:justify-items-center xl:items-end xl:content-end'>
 
         <div className='w-[327px] sm:w-[450px] flex flex-col items-center xl:items-start'>
           <h5 className='heading--5 uppercase'>
@@ -15,7 +15,7 @@ const Home = () => {
             Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!
           </p>
         </div>
-        <div className='group w-[150px] h-[150px] sm:w-60 sm:h-60 xl:w-[272px] xl:h-[272px] mt-12 sm:mt-[150px] md:mt-32 bg-white rounded-full flex items-center justify-center cursor-pointer'>
+        <div className='group w-[150px] h-[150px] sm:w-60 sm:h-60 xl:w-[272px] xl:h-[272px] mt-12 bg-white rounded-full flex items-center justify-center cursor-pointer'>
           <p className='buttonText'>Explore</p>
           <div className='w-[150px] h-[150px] sm:w-60 sm:h-60 xl:w-[272px] xl:h-[272px] absolute rounded-full group-hover:scale-[1.45] group-hover:bg-white opacity-[0.1] z-10'></div>
         </div>

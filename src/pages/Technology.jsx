ab84a880e120915ec9c68521ceef44d785d8f061
felpfsf <feferreira.sf@gmail.com>
2022-07-14 17:59:31 -0300

    correcting image path name

--- src/pages/Technology.jsx
+++ src/pages/Technology.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Technology = () => {
   return (
-    <div className='w-full h-screen bg-[url("/assets/technology/background-technology-mobile.jpg")] sm:bg-[url("/assets/technology/background-technology-tablet.jpg") xl:bg-[url("/assets/technology/background-technology-desktop.jpg")] bg-cover bg-center'>Technology</div>
+    <div className='w-full h-screen bg-[url("/assets/technology/background-technology-mobile.jpg")] sm:bg-[url("/assets/technology/background-technology-tablet.jpg")] xl:bg-[url("/assets/technology/background-technology-desktop.jpg")] bg-cover bg-center'>Technology</div>
   )
 }
 

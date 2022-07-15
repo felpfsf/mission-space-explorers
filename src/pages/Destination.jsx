16151de38d65b2de4d91091ee93252b2afe50062
felpfsf <feferreira.sf@gmail.com>
2022-07-15 14:12:54 -0300

    refactoring

--- src/pages/Destination.jsx
+++ src/pages/Destination.jsx
@@ -3,20 +3,28 @@ import data from '../data/data.json'
 const Destination = () => {
   return (
     <div className='w-full h-screen bg-[url("/assets/destination/background-destination-mobile.jpg")] sm:bg-[url("/assets/destination/background-destination-tablet.jpg")] xl:bg-[url("/assets/destination/background-destination-desktop.jpg")] bg-cover bg-center overflow-auto xl:overflow-hidden flex flex-col items-center justify-center'>
-      <h5 className='heading--5 uppercase'>
+      <h5 className='pagesTitle text-white uppercase'>
         <strong className='mr-[11px]'>01</strong>
         Pick your destination
       </h5>
       <img className='w-[170px] h-[170px]' src={data.destinations[0].images.png} alt="" />
       <div className='w-[327px] flex flex-col items-center'>
-        <ul className='heading--5 uppercase flex gap-4'>
+        <ul className='destMenu uppercase flex gap-[26px]'>
           {data.destinations.map((destination, index) =>
             <li key={index}>{destination.name}</li>
           )}
         </ul>
-        <h1 className='heading--1 uppercase'>{data.destinations[0].name}</h1>
-        <p className='paragraph text-center'>{data.destinations[0].description}</p>
-        <div className='w-full h-[1px] my-8 border border-white/25' ></div>
+        <h1 className='destName uppercase'>{data.destinations[0].name}</h1>
+        <p className='paragraphText text-center'>{data.destinations[0].description}</p>
+        <div className='w-full h-[1px] my-8 bg-[#383B4B]' ></div>
+        <div>
+          <h6>AVG. DISTANCE</h6>
+          <h6>{data.destinations[0].distance}</h6>
+        </div>
+        <div>
+          <h6>Est. travel time</h6>
+          <h6>{data.destinations[0].travel}</h6>
+        </div>
       </div>
     </div>
   )

5f995fe3e7cbc0ed50f9980a63a7476514973178
felpfsf <feferreira.sf@gmail.com>
2022-07-18 14:33:05 -0300

    crew layout

--- src/pages/Crew.jsx
+++ src/pages/Crew.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import data from '../data/data.json'
 
 const Crew = () => {
   return (
-    <div className='w-full h-screen crewBackgrounds'>
-      Crew
+    <div className='w-full h-screen crewBackgrounds overflow-auto'>
+      <div className=''>
+        <h5 className='pagesTitle text-white uppercase mb-8'><strong className='mr-[11px] opacity-25'>02</strong>Meet your crew</h5>
+      </div>
     </div>
   )
 }

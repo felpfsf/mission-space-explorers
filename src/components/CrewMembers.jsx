16355f0f9b353ae44e3ac184bb7781a934744e6b
felpfsf <feferreira.sf@gmail.com>
2022-07-18 19:35:49 -0300

    adjusting bio field size

--- src/components/CrewMembers.jsx
+++ src/components/CrewMembers.jsx
@@ -6,7 +6,7 @@ const CrewMembers = ({index}) => {
       <div className='flex flex-col items-center xl:items-start xl:mb-32'>
         <h5 className='crewRole uppercase'>{data.crew[index].role}</h5>
         <h1 className='crewTechName uppercase'>{data.crew[index].name}</h1>
-        <p className='paragraphText text-center xl:text-left my-4 w-[444px]'>{data.crew[index].bio}</p>
+        <p className='paragraphText text-center xl:text-left my-4 max-w-[444px]'>{data.crew[index].bio}</p>
       </div>
     </>
   )

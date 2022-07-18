eb1c5fa78e047cca8eeda9fb4644509aec57de02
felpfsf <feferreira.sf@gmail.com>
2022-07-18 18:05:29 -0300

    correcting custom font-size

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
         // Font Dynamic Size
         btnTextDynamic: 'clamp(1.25rem, 1.0000rem + 1.1111vw, 2rem)',
         mainTitleSize: 'clamp(6.25rem, 5.2083rem + 4.6296vw, 9.375rem)',
-        pagesTitleSize: 'clamp(1rem, 0.9583rem + 0.1852vw, 1.125rem)',
+        pagesTitleSize: 'clamp(1rem, 0.7500rem + 1.1111vw, 1.75rem)',
         destNameSize: 'clamp(5rem, 4.5833rem + 1.8519vw, 6.25rem)',
         destMenuSize: 'clamp(0.875rem, 0.8333rem + 0.1852vw, 1rem)',
         crewTitleSize: 'clamp(1.5rem, 0.8333rem + 2.9630vw, 3.5rem)',

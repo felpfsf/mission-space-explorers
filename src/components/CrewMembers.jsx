7c2936d85c3df1aa2e4aca75e666ae23b0e1894d
felpfsf <feferreira.sf@gmail.com>
2022-07-18 17:45:41 -0300

    crew section

---
+++ src/components/CrewMembers.jsx
@@ -0,0 +1,15 @@
+import data from '../data/data.json'
+
+const CrewMembers = ({index}) => {
+  return (
+    <>
+      <div className='flex flex-col items-center xl:items-start xl:mb-32'>
+        <h5 className='crewRole uppercase'>{data.crew[index].role}</h5>
+        <h1 className='crewTechName uppercase'>{data.crew[index].name}</h1>
+        <p className='paragraphText text-center xl:text-left my-4'>{data.crew[index].bio}</p>
+      </div>
+    </>
+  )
+}
+
+export default CrewMembers
--- src/pages/Crew.jsx
+++ src/pages/Crew.jsx
@@ -1,31 +1,34 @@
+import { useState } from 'react'
+import CrewMembers from '../components/CrewMembers'
+import CrewPics from '../components/CrewPics'
 import data from '../data/data.json'
 
 const Crew = () => {
+  const [content, setContent] = useState(0)
   return (
     <div className='w-full h-screen crewBackgrounds overflow-auto'>
       {/* <div className='absolute top-0 left-0 w-full h-screen bg-black/10'></div> */}
-      <div className='w-[327px] md:w-[458px] mt-[88px] md:mt-[136px] mx-auto grid justify-items-center'>
-        <h5 className='pagesTitle text-white uppercase mb-8 md:-order-1'>
+      <div className='w-[327px] md:w-[458px] xl:w-full mt-[88px] md:mt-[136px] xl:mt-[150px] mx-auto grid justify-items-center xl:grid-cols-2 xl:auto-rows-auto'>
+        {/* page title */}
+        <h5 className='pagesTitle text-white uppercase mb-8 md:-order-1 xl:order-none xl:place-self-center'>
           <strong className='mr-[11px] opacity-25'>02</strong>
           Meet your crew
         </h5>
-        <img className='w-[177px] md:w-[456px]' src={data.crew[0].images.png} alt="" />
+        {/* crewPics */}
+        <CrewPics index={content} />
+        {/* separator mobile */}
         <div className='md:hidden w-full h-[1px] bg-[#64687c]' ></div>
-        <div className='my-8 flex flex-col md:flex-col-reverse items-center md:-order-1'>
+        <div className='xl:w-[488px] my-8 flex flex-col md:flex-col-reverse items-center md:-order-1 xl:order-none xl:items-start xl:place-self-center'>
           {/* buttons */}
           <ul className='mb-8 flex items-center gap-4'>
-            {data.crew.map((crewMember) =>
-              <li key={crewMember}>
-                <div className='w-4 h-4 hover:bg-white bg-white/20 rounded-full cursor-pointer'></div>
+            {data.crew.map((crewMember, index) =>
+              <li key={index}>
+                <div className='w-4 h-4 hover:bg-white bg-white/20 rounded-full cursor-pointer' onClick={() => setContent(index)}></div>
               </li>
             )}
           </ul>
           {/* content */}
-          <div className='flex flex-col items-center'>
-            <h5 className='crewRole uppercase'>{data.crew[0].role}</h5>
-            <h1 className='crewTechName uppercase'>{data.crew[0].name}</h1>
-            <p className='paragraphText text-center xl:text-left my-4'>{data.crew[0].bio}</p>
-          </div>
+          <CrewMembers index={content} />
         </div>
       </div>
     </div>

38b62fc303fc59696d03cf22c2462cd1b27af0f2
felpfsf <feferreira.sf@gmail.com>
2022-07-18 18:16:14 -0300

    adjusting content size

--- src/components/CrewPics.jsx
+++ src/components/CrewPics.jsx
@@ -3,7 +3,7 @@ import data from '../data/data.json'
 const CrewPics = ({index}) => {
   return (
     <>
-      <img className='w-[177px] md:w-[456px]  xl:row-span-3 xl:self-end' src={data.crew[index].images.png} alt="" />
+      <img className='w-[177px] md:w-[456px] xl:w-[550px] xl:row-span-3 xl:self-end row-span-3' src={data.crew[index].images.png} alt="" />
 
     </>
   )
--- src/pages/Crew.jsx
+++ src/pages/Crew.jsx
@@ -8,9 +8,9 @@ const Crew = () => {
   return (
     <div className='w-full h-screen crewBackgrounds overflow-auto'>
       {/* <div className='absolute top-0 left-0 w-full h-screen bg-black/10'></div> */}
-      <div className='w-[327px] md:w-[458px] xl:w-full mt-[88px] md:mt-[136px] xl:mt-[150px] mx-auto grid justify-items-center xl:grid-cols-2 xl:auto-rows-auto'>
+      <div className='w-[327px] md:w-[458px] xl:w-full h-[85%] mt-[88px] md:mt-[136px] mx-auto grid justify-items-center xl:grid-cols-2 xl:grid-rows-2'>
         {/* page title */}
-        <h5 className='pagesTitle text-white uppercase mb-8 md:-order-1 xl:order-none xl:place-self-center'>
+        <h5 className='pagesTitle text-white uppercase mb-8 md:-order-1 xl:order-none xl:place-self-center row-end-1 col-start-1 col-end-1'>
           <strong className='mr-[11px] opacity-25'>02</strong>
           Meet your crew
         </h5>
@@ -18,7 +18,7 @@ const Crew = () => {
         <CrewPics index={content} />
         {/* separator mobile */}
         <div className='md:hidden w-full h-[1px] bg-[#64687c]' ></div>
-        <div className='xl:w-[488px] my-8 flex flex-col md:flex-col-reverse items-center md:-order-1 xl:order-none xl:items-start xl:place-self-center'>
+        <div className='xl:w-[488px] my-8 flex flex-col md:flex-col-reverse items-center md:-order-1 xl:order-none xl:items-start xl:place-self-center row-start-1 row-end-3'>
           {/* buttons */}
           <ul className='mb-8 flex items-center gap-4'>
             {data.crew.map((crewMember, index) =>
